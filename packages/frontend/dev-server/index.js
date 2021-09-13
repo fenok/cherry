8c1bbb11db2486fb8a2a3a3c6d131bd411dbdfa8
Leonid Fenko <fenok2112@gmail.com>
2021-09-14 00:21:32 +0300

    Add option to enable webpack-hot-middleware logs

--- packages/frontend/dev-server/index.js
+++ packages/frontend/dev-server/index.js
@@ -15,27 +15,39 @@ async function start() {
     const app = express();
 
     app.use(webpackDevMiddleware(compiler, { serverSideRender: true }));
-    app.use(webpackHotMiddleware(compiler));
+    app.use(webpackHotMiddleware(compiler, { log: params.l ? console.log : false }));
     app.use(ssrMiddleware);
 
     const port = await getPort({ port: getPort.makeRange(3000, 3100) });
 
     app.listen(port, async () => {
         console.log(`Cherry dev server is up at http://localhost:${port}`);
+        logOptions();
 
         if (params.o) {
-            console.log("Opening browser tab...");
             await open(`http://localhost:${port}`);
-        } else {
-            console.log("Use -o option to automatically open a browser tab.");
-        }
-
-        if (params.p) {
-            console.log("Warning: using production build.");
-        } else {
-            console.log("Use -p option to switch to production build.");
         }
     });
+}
+
+function logOptions() {
+    if (params.p) {
+        console.log("Warning: using production build.");
+    } else {
+        console.log("Use -p option to switch to production build.");
+    }
+
+    if (params.l) {
+        console.log("Showing webpack-hot-middleware logs...");
+    } else {
+        console.log("Use -l option to enable webpack-hot-middleware logs.");
+    }
+
+    if (params.o) {
+        console.log("Opening browser tab...");
+    } else {
+        console.log("Use -o option to automatically open a browser tab.");
+    }
 }
 
 void start();
